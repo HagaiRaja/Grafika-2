ccad417c454a043adc5af31c85a237f8a89b0802
TheSecret3003 <ekasurya1410@gmail.com>
2019-02-05 17:23:11 +0700

    Modified Draw

--- draw.c
+++ draw.c
@@ -255,17 +255,139 @@ void drawLine(point p, point q, color* c)
 // Drawing Canon with given center point with line color c
 void drawCanon(point center, color* c)
 {
-
+	point sudut1;
+	sudut1.x = center.x - 20;
+	sudut1.y = center.y - 50;
+	point sudut2;
+	sudut2.x = center.x + 20;
+	sudut2.y = center.y - 50;
+	point sudut3;
+	sudut3.x = center.x - 20;
+	sudut3.y = center.y + 50;
+	point sudut4;
+	sudut4.x = center.x + 20;
+	sudut4.y = center.y + 50;
+	point sudut5;
+	sudut5.x = center.x - 10;
+	sudut5.y = center.y - 100;
+	point sudut6;
+	sudut6.x = center.x + 10;
+	sudut6.y = center.y - 100;
+	drawLine(sudut1,sudut5,c);
+	drawLine(sudut1,sudut3,c);
+	drawLine(sudut3,sudut4,c);
+	drawLine(sudut4,sudut2,c);
+	drawLine(sudut2,sudut6,c);
+	drawLine(sudut5,sudut6,c);
 }
 
 // Drawing Bullet with given center point with line color c
 void drawBullet(point center, color* c)
 {
-
+	point sudut1;
+	sudut1.x = center.x;
+	sudut1.y = center.y - 25;
+	point sudut2;
+	sudut2.x = center.x - 20;
+	sudut2.y = center.y - 10;
+	point sudut3;
+	sudut3.x = center.x - 15;
+	sudut3.y = center.y + 20;
+	point sudut4;
+	sudut4.x = center.x + 15;
+	sudut4.y = center.y + 20;
+	point sudut5;
+	sudut5.x = center.x + 20;
+	sudut5.y = center.y - 10;
+	drawLine(sudut1,sudut3,c);
+	drawLine(sudut1,sudut4,c);
+	drawLine(sudut2,sudut4,c);
+	drawLine(sudut2,sudut5,c);
+	drawLine(sudut3,sudut5,c);
 }
 
 // Drawing Plane with given center point with line color c
 void drawPlane(point center, color* c)
 {
+	point sudut1;
+	sudut1.x = center.x - 175;
+	sudut1.y = center.y - 20;
+	point sudut2;
+	sudut2.x = center.x - 175;
+	sudut2.y = center.y + 20;
+	point sudut3;
+	sudut3.x = center.x - 135;
+	sudut3.y = center.y - 50;
+	point sudut4;
+	sudut4.x = center.x - 135;
+	sudut4.y = center.y + 50;
+	point sudut5;
+	sudut5.x = center.x - 90;
+	sudut5.y = center.y - 50;
+	point sudut6;
+	sudut6.x = center.x - 60;
+	sudut6.y = center.y - 120;
+	point sudut7;
+	sudut7.x = center.x - 15;
+	sudut7.y = center.y - 120;
+	point sudut8;
+	sudut8.x = center.x - 45;
+	sudut8.y = center.y - 50;
+	point sudut9;
+	sudut9.x = center.x - 90;
+	sudut9.y = center.y + 50;
+	point sudut10;
+	sudut10.x = center.x - 60;
+	sudut10.y = center.y + 120;
+	point sudut11;
+	sudut11.x = center.x - 15;
+	sudut11.y = center.y + 120;
+	point sudut12;
+	sudut12.x = center.x - 45;
+	sudut12.y = center.y + 50;
+	point sudut13;
+	sudut13.x = center.x + 100;
+	sudut13.y = center.y - 50;
+	point sudut14;
+	sudut14.x = center.x + 125;
+	sudut14.y = center.y - 120;
+	point sudut15;
+	sudut15.x = center.x + 175;
+	sudut15.y = center.y - 120;
+	point sudut16;
+	sudut16.x = center.x + 150;
+	sudut16.y = center.y - 50;
+	point sudut17;
+	sudut17.x = center.x + 100;
+	sudut17.y = center.y + 50;
+	point sudut18;
+	sudut18.x = center.x + 125;
+	sudut18.y = center.y + 120;
+	point sudut19;
+	sudut19.x = center.x + 175;
+	sudut19.y = center.y + 120;
+	point sudut20;
+	sudut20.x = center.x + 150;
+	sudut20.y = center.y + 50;
+	drawLine(sudut1,sudut2,c);
+	drawLine(sudut1,sudut3,c);
+	drawLine(sudut2,sudut4,c);
+	drawLine(sudut3,sudut5,c);
+	drawLine(sudut5,sudut6,c);
+	drawLine(sudut6,sudut7,c);
+	drawLine(sudut7,sudut8,c);
+	drawLine(sudut8,sudut13,c);
+	drawLine(sudut13,sudut14,c);
+	drawLine(sudut14,sudut15,c);
+	drawLine(sudut15,sudut16,c);
+	drawLine(sudut4,sudut9,c);
+	drawLine(sudut9,sudut10,c);
+	drawLine(sudut10,sudut11,c);
+	drawLine(sudut11,sudut12,c);
+	drawLine(sudut12,sudut17,c);
+	drawLine(sudut17,sudut18,c);
+	drawLine(sudut18,sudut19,c);
+	drawLine(sudut19,sudut20,c);
+	drawLine(sudut16,sudut20,c);
 
 }
--- draw_driver.c
+++ draw_driver.c
@@ -9,40 +9,47 @@ int main(int argc, char const *argv[])
 	color white = {255,255,255,0};
 	set_background(&white);
 
+	// CEK DRAW LINE METHOD
 	point a = {300,300};
-	point b = {300,200};
-	point c = {400,200};
-	point d = {400,300};
-	point e = {400,400};
-	point f = {300,400};
-	point g = {200,400};
-	point h = {200,300};
-	point i = {200,200};
-	drawLine(a, b, &black);
-	drawLine(a, c, &black);
-	drawLine(a, d, &black);
-	drawLine(a, e, &black);
-	drawLine(a, f, &black);
-	drawLine(a, g, &black);
-	drawLine(a, h, &black);
-	drawLine(a, i, &black);
+	point b = {500,500};
+	point c = {200,600};
+	// point b = {300,200};
+	// point c = {400,200};
+	// point d = {400,300};
+	// point e = {400,400};
+	// point f = {300,400};
+	// point g = {200,400};
+	// point h = {200,300};
+	// point i = {200,200};
+	// drawLine(a, b, &black);
+	// drawLine(a, c, &black);
+	// drawLine(a, d, &black);
+	// drawLine(a, e, &black);
+	// drawLine(a, f, &black);
+	// drawLine(a, g, &black);
+	// drawLine(a, h, &black);
+	// drawLine(a, i, &black);
 
 
-	point j = {350,200};
-	point k = {400,250};
-	point l = {400,350};
-	point m = {350,400};
-	point n = {250,400};
-	point o = {200,350};
-	point p = {200,250};
-	point q = {250,200};
-	drawLine(a, j, &black);
-	drawLine(a, k, &black);
-	drawLine(a, l, &black);
-	drawLine(a, m, &black);
-	drawLine(a, n, &black);
-	drawLine(a, o, &black);
-	drawLine(a, p, &black);
+	// point j = {350,200};
+	// point k = {400,250};
+	// point l = {400,350};
+	// point m = {350,400};
+	// point n = {250,400};
+	// point o = {200,350};
+	// point p = {200,250};
+	// point q = {250,200};
+	// drawLine(a, j, &black);
+	// drawLine(a, k, &black);
+	// drawLine(a, l, &black);
+	// drawLine(a, m, &black);
+	// drawLine(a, n, &black);
+	// drawLine(a, o, &black);
+	// drawLine(a, p, &black);
+
+	drawCanon(a, &black);
+	drawBullet(b, &black);
+	drawPlane(c, &black);
 
 	close_buffer();
 	return 0;
